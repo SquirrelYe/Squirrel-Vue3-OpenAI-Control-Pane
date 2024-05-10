18748f1cfaf785c92c571fcea28fce1a6ce656a8
@willye <willye@tencent.com>
2024-05-10 09:28:26 +0800

    refactor: 💡 清理无关代码

--- src/view/pages/about/About.vue
+++ src/view/pages/about/About.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="setting">
-    <el-container>
-      <el-header>
-        <transition name="el-zoom-in-top">
-          <div v-show="isShowProfile" class="transition-box">
-            <h3>JUN CHEN MO</h3>
-          </div>
-        </transition>
-      </el-header>
-
-      <el-main>
-        <transition name="el-zoom-in-top">
-          <div v-show="isShowProfile" class="transition-box">
-            <span>
-              现已接入OpenAI的Models API、Completions API、Chat API、Audio API、Images API、Files API、Fine-tunes API后续会添加更多有意思的功能进去，希望大家给我的GitHub点个小小的星星，
-              大家如果有什么好的想法可以在GitHub中提出来，觉得项目不错的朋友可以打赏一波，感谢支持。
-            </span>
-            <div><a href="https://space.bilibili.com/326625155?spm_id_from=333.337.0.0">BliBili</a>---<a href="https://github.com/202252197/ChatGPT_JCM">GitHub</a></div>
-            <div>
-              <h3>愿半生编码，如一生老友</h3>
-            </div>
-          </div>
-        </transition>
-      </el-main>
-    </el-container>
+    <el-container> </el-container>
   </div>
 </template>
 
